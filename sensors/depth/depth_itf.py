4ac2b679904ab511197d4780b6ff290db98ed063
Dominik Wasiółka <dominowasy@gmail.com>
2019-02-26 22:51:10 +0100

    add depth sensor interface

---
+++ sensors/depth/depth_itf.py
@@ -0,0 +1,17 @@
+"""
+Module includes interface for depth sensor
+"""
+import abc
+from sensors.base_sensor_itf import IBaseSensor
+
+
+class IDepthSensor(metaclass=abc.ABCMeta):
+    """
+    Class for getting data from depth sensor by use of virtual file
+    """
+    @abc.abstractmethod
+    def get_depth(self):
+        """
+        :return: single integer which represent depth of ROV in cm
+        """
+        pass
