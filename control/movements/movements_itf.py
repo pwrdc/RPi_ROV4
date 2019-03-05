2bdf6e64de5a3213d448e18a835654cb9b07e149
DominikWasiolka <dominowasy@gmail.com>
2019-03-05 01:43:46 +0100

    Add inheritance form BaseControler and docstings fix

--- control/movements/movements_itf.py
+++ control/movements/movements_itf.py
@@ -2,9 +2,10 @@
 Module includes IMovements
 """
 import abc
+from control.base_controller import BaseController
 
 
-class IMovements(metaclass=abc.ABCMeta):
+class IMovements(BaseController, metaclass=abc.ABCMeta):
     """
     Interfce for control ROV movements
     """
@@ -12,9 +13,9 @@ class IMovements(metaclass=abc.ABCMeta):
     def set_lin_velocity(self, front, right, up):
         """
         Set linear velocity as 100% of engines power
-        @param: front float in range [-1, 1], case negative value move back
-        @param: right float in range [-1, 1], case negative value move down
-        @param: up float in range [-1,1], case negative value move down
+        :param front: float in range [-1, 1], case negative value move back
+        :param right: float in range [-1, 1], case negative value move down
+        :param up: float in range [-1,1], case negative value move down
         """
         pass
 
@@ -22,9 +23,9 @@ class IMovements(metaclass=abc.ABCMeta):
     def set_ang_velocity(self, roll, pitch, yaw):
         """
         Set angular velocity as 100% of engines power
-        @param: roll float in range [-1, 1], case negative - reverse direction
-        @param: pitch float in range [-1, 1], case negative - reverse direction
-        @param: yaw float in range [-1,1], case negative - reverse direction
+        :param roll: float in range [-1, 1], case negative - reverse direction
+        :param pitch: float in range [-1, 1], case negative - reverse direction
+        :param yaw: float in range [-1,1], case negative - reverse direction
         """
         pass
 
@@ -32,9 +33,9 @@ class IMovements(metaclass=abc.ABCMeta):
     def move_distance(self, front, right, up):
         """
         Make precise linear movement, valeues in meters
-        @param: front float in range [-10, 10], case negative value move back
-        @param: right float in range [-10, 10], case negative value move down
-        @param: up float in range [-10,10], case negative value move down
+        :param front: float in range [-10, 10], case negative value move back
+        :param right: float in range [-10, 10], case negative value move down
+        :param up: float in range [-10,10], case negative value move down
 
         Not shure if it is going to work correctly
         """
@@ -44,9 +45,9 @@ class IMovements(metaclass=abc.ABCMeta):
     def rotate_angle(self, roll, pitch, yaw):
         """
         Make precise angular movement
-        @param: roll float in range [-360, 360], case negative - reverse direction
-        @param: pitch float in range [-360, 360], case negative - reverse direction
-        @param: yaw flaot in range [-360, 360], case negative - reverse direction
+        :param roll: float in range [-360, 360], case negative - reverse direction
+        :param pitch: float in range [-360, 360], case negative - reverse direction
+        :param yaw: flaot in range [-360, 360], case negative - reverse direction
         """
         pass
 
