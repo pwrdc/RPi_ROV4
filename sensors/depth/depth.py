eaf3cd177d3c9d4067a5898c3bf8819dfd1367ed
DominikWasiolka <dominowasy@gmail.com>
2019-03-25 23:41:25 +0100

    Add client to depth.py

--- sensors/depth/depth.py
+++ sensors/depth/depth.py
@@ -1,16 +1,24 @@
 from sensors.depth.depth_itf import IDepthSensor
+from communication.rpi_drivers.rov_comm import Client
+from communication.rpi_drivers.settings import depth_client_port
 
 
 class DepthSensor(IDepthSensor):
     '''
+    Depth sensor
+    Use rpi server to accessing data from sensor
     '''
+    def __init__(self, main_logger=None, local_log=False, log_directory="", log_timing=0.5):
+        super().__init__(main_logger, local_log, log_directory, log_timing)
+        self.client = Client(depth_client_port)
+
     @IDepthSensor.multithread_method
     def get_depth(self):
         '''
         Get current depth
         :return: depth as single integer
         '''
-        pass
+        return self.client.get_data()
 
-    def getter2msg():
-        return str(get_depth())
+    def getter2msg(self):
+        return str(self.get_depth())
