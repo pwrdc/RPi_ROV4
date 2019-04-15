6f2333fd305ad04a6a7855a07154080155d49111
krzysiekdzi <krzysztofdziuba97@gmail.com>
2019-04-15 17:21:15 +0200

    Found error in ahrs.py. general_test.py went ok

--- sensors/ahrs/ahrs.py
+++ sensors/ahrs/ahrs.py
@@ -26,8 +26,8 @@ class AHRS(BaseSensor,IAHRS):
         received = self.get_data()
         print(received)
         output = {}
-        if received != None and received[0] == '{':
-            received = ast.literal_eval(received)
+        if received != None:
+            #received = ast.literal_eval(received)
             output['yaw'] = received['yaw']
             output['pitch'] = received['pitch']
             output['roll'] = received['roll']
@@ -44,8 +44,8 @@ class AHRS(BaseSensor,IAHRS):
         received = self.get_data()
         output = {}
         print(received)
-        if received != None and received[0] == '{':
-            received = ast.literal_eval(received)
+        if received != None:
+            #received = ast.literal_eval(received)
             output['lineA_x'] = received['lineA_x']
             output['lineA_y'] = received['lineA_y']
             output['lineA_z'] = received['lineA_z']
@@ -62,8 +62,8 @@ class AHRS(BaseSensor,IAHRS):
         received = self.get_data()
         output = {}
         print(received)
-        if received != None and received[0] == '{':
-            received = ast.literal_eval(received)
+        if received != None:
+            #received = ast.literal_eval(received.decode("utf-8"))
             output['angularA_x'] = received['angularA_x']
             output['angularA_y'] = received['angularA_x']
             output['angularA_z'] = received['angularA_x']
