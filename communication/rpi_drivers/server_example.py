7f0489e77ca31042fadbfd1c964c525909110ceb
DominikWasiolka <dominowasy@gmail.com>
2019-05-02 08:39:18 +0200

    remove files, which was moved to Electronics_rpi repository

--- communication/rpi_drivers/server_example.py
+++
@@ -1,9 +0,0 @@
-import rov_comm
-import datetime
-
-CLIENT_PORT = 1234
-DRIVER_PORT = 1235
-
-server = rov_comm.ZMQ_Server(data_template=0, client_port=CLIENT_PORT, driver_port=DRIVER_PORT)
-
-server.run()
