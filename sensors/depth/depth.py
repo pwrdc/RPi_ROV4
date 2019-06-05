5c8204279b8f9cbed82f36a33f09d0afb256545f
krzysiekdzi <krzysztofdziuba97@gmail.com>
2019-06-05 21:23:55 +0200

    Changed depth sensor client to Pyro4

--- sensors/depth/depth.py
+++ sensors/depth/depth.py
@@ -1,21 +1,26 @@
 from sensors.depth.depth_itf import IDepthSensor
-from sensors.base_sensor import BaseSensor
+from control.base import Base
+import Pyro4
 
 
-class DepthSensor(BaseSensor,IDepthSensor):
+class DepthSensor(Base,IDepthSensor):
     '''
     Depth sensor
     Use rpi server to accessing data from sensor
     '''
     #@IDepthSensor.multithread_method
+    def __init__(self, main_logger=None, local_log=False):
+        super(DepthSensor, self).__init__(main_logger=main_logger, local_log=local_log)
+        Pyro4.locateNS()
+        self.depth_server = Pyro4.Proxy("PYRONAME:depth_server")
     def get_depth(self):
         '''
         Get current depth
         :return: depth as single integer
         '''
-        return self.get_data()
+        return self.depth_server.get_depth()
 
     def getter2msg(self):
         #TODO this is temporary solution change to str(self.get_depth())
-        return str(self.get_data())
+        return str(self.depth_server.get_depth())
 
