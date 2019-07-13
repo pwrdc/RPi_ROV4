e1bf7f5d359aa96abfae33505ed42f1f560f3e92
DominikWasiolka <dominowasy@gmail.com>
2019-07-13 12:45:04 +0200

    replace 0 wit 0.0 in movements server

--- communication/rpi_drivers/movements_server.py
+++ communication/rpi_drivers/movements_server.py
@@ -2,12 +2,12 @@ import rov_comm
 import ports
 
 example ={
-    'front':0,
-    'right':0,
-    'up':0,
-    'roll':0,
-    'pitch':0,
-    'yaw':0
+    'front': 0.0,
+    'right': 0.0,
+    'up': 0.0,
+    'roll': 0.0,
+    'pitch': 0.0,
+    'yaw': 0.0
 }
 server = rov_comm.ZMQ_Server(example, ports.ENGINE_SLAVE_PORT, ports.ENGINE_MASTER_PORT)
 
