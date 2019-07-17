a3fe02cc4233f1e967b0fb7a41c3c5acf4e2387b
DominikWasiolka <dominowasy@gmail.com>
2019-07-17 18:41:48 +0200

    change python3.6 to pyhton3 in run_servers

--- communication/rpi_drivers/run_servers.sh
+++ communication/rpi_drivers/run_servers.sh
@@ -1,5 +1,5 @@
 !/bin/bash
-python3.6 -m Pyro4.naming -n 192.168.0.102 &
-python3.6 movements_server.py &
-python3.6 ahrs_server.py &
-python3.6 depth_server.py &
+python3 -m Pyro4.naming -n 192.168.0.102 &
+python3 movements_server.py &
+python3 ahrs_server.py &
+python3 depth_server.py &
