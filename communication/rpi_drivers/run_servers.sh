74396222142ed32a63fdfed90b332d64195c9668
DominikWasiolka <dominowasy@gmail.com>
2019-07-18 05:42:09 +0200

    add distance server to run_servers.sh

--- communication/rpi_drivers/run_servers.sh
+++ communication/rpi_drivers/run_servers.sh
@@ -1,5 +1,9 @@
-!/bin/bash
-python3 -m Pyro4.naming -n 192.168.0.102 &
+#!/bin/bash
+python3 -m Pyro4.naming -n 192.168.0.100 &
 python3 movements_server.py &
 python3 ahrs_server.py &
 python3 depth_server.py &
+python3 distance_server.py &
+#python3 manipulator_server.py &
+#python3 torpedo_server.py &
+
