f9dbd52e26ec05428e945739a3afd504dafe1f55
DominikWasiolka <dominowasy@gmail.com>
2019-09-16 12:10:43 +0200

    distance: return previous value if data server error

--- sensors/distance/distance.py
+++ sensors/distance/distance.py
@@ -4,13 +4,21 @@ from sensors.base_sensor import BaseSensor
 class DistanceSensor(BaseSensor,IDistanceSensor):
     '''
     '''
+    prev_val = 3500
     #@Base.multithread_method
     def get_front_distance(self):
         '''
         Get distance from obstacle in front of ROV
 
-        :return: distance as single integer
+        :return: distance as single float
         '''
-        return self.get_data()
+        print(self.prev_val)
+        val = self.get_data()
+        try:
+            self.prev_val = val
+            return float(self.get_data())
+        except Exception:
+            return self.prev_val
+
     def getter2msg(self):
         return str(self.get_data())
