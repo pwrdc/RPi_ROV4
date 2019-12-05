8336d2e581d4d8e0a6f98bb39c56f9542a87befc
krzysiekdzi <krzysztofdziuba97@gmail.com>
2019-12-05 17:16:57 +0100

    Recording route and playing it

---
+++ communication/communication_record.py
@@ -0,0 +1,204 @@
+import Pyro4
+import threading
+
+#Controls interfaces imports for inheritance
+from control.lights.lights_itf import ILights
+from control.manipulator.manipulator_itf import IManipulator
+from control.movements.movements_itf import IMovements
+from control.torpedoes.torpedoes_itf import ITorpedoes
+
+#Sensors interfaces imports for inheritance
+from sensors.ahrs.ahrs_itf import IAHRS
+from sensors.depth.depth_itf import IDepthSensor
+from sensors.distance.distance_itf import IDistanceSensor
+from sensors.hydrophones.hydrophones_itf import IHydrophonesPair
+
+@Pyro4.expose
+class Communication(threading.Thread, ILights, IManipulator, IMovements,
+                    ITorpedoes, IAHRS, IDepthSensor, IDistanceSensor, IHydrophonesPair):
+    '''
+    This class is responsible of finding Pyro4 nameserver,
+    registering itself in there and providing all methods
+    what classes passed from main thread offer.
+    '''
+    def __init__(self, sensors_refs, rpi_address, main_logger=None, log_directory=''):
+        '''
+        Starting new thread, starting Pyro4 server,
+        finding Pyro4 nameserver, registering 'self' in the nameserver,
+        running Pyro4 server loop
+        '''
+        self.main_logger = main_logger
+        self.log_directory = log_directory
+        threading.Thread.__init__(self)
+        self.sensors_refs = sensors_refs
+
+        #sensor_refs is used to store references to all objects passed from main thread
+
+
+        daemon = Pyro4.Daemon(str(rpi_address))
+
+        try:
+            name_server = Pyro4.locateNS()  #It's possible to pass NS IP address to locateNS() (as string)
+            self.m_path = open("movements_path.csv", "w")
+            #Tries to find Pyro nameserver
+
+        except Exception as err:
+            main_logger.log("Most probably couldn't find name server "+str(err))
+
+        try:
+            name_server.register('RPI_communication',daemon.register(self))
+            #Tries to register self object in Pyro4 nameserver as 'RPI_communication'
+            print('RPI registered in Pyro')
+
+        except Exception as err:
+            main_logger.log('Problem with communication '+str(err))
+
+        main_logger.log('Communication server set correctly')
+        daemon.requestLoop()
+        #Starting Pyro4 server loop
+
+    def power_lights(self, power_supplied):
+        self.sensors_refs['Lights'].power_lights(power_supplied)
+
+    # Movements
+    def set_lin_velocity(self, front, right, up):
+        self.sensors_refs['Movements'].set_lin_velocity(
+            front, right, up
+        )
+
+    def set_ang_velocity(self, roll, pitch, yaw):
+        self.sensors_refs['Movements'].set_ang_velocity(
+            roll, pitch, yaw
+        )
+
+    def move_distance(self, front, right, up):
+        self.sensors_refs['Movements'].move_distance(
+            front, right, up
+        )
+
+    def rotate_angle(self, roll, pitch, yaw):
+        #self.pid_set_yaw(self.get_yaw()+yaw)
+        self.sensors_refs['Movements'].rotate_angle(
+            roll, pitch, yaw
+        )
+
+    def set_engine_driver_values(self, front, right, up,
+                                 roll, pitch, yaw):
+        for i in [front,right,up,roll,pitch,yaw]:
+            self.m_path.write(i)
+        self.m_path.write('\n')
+        self.sensors_refs['Movements'].set_engine_driver_values(
+            front, right, up, roll, pitch, yaw
+        )
+        #Movements saving
+
+
+
+    # Movements pid - depth
+    def pid_depth_turn_on(self):
+        self.sensors_refs['Movements'].pid_depth_turn_on()
+
+    def pid_depth_turn_off(self):
+        self.sensors_refs['Movements'].pid_depth_turn_off()
+
+    def pid_hold_depth(self):
+        self.sensors_refs['Movements'].pid_hold_depth()
+
+    def pid_depth_set_params(self, kp, ki, kd):
+        self.sensors_refs['Movements'].pid_depth_set_params(kp, ki, kd)
+
+    def pid_set_depth(self, depth):
+        """
+        :param: depth - float - target depth for PID
+        """
+        self.sensors_refs['Movements'].pid_set_depth(depth)
+
+    # for GUI
+    def get_pid_depth_error(self):
+        return self.sensors_refs['Movements'].pid_depth_get_error()
+
+    def get_pid_depth_output(self):
+        return self.sensors_refs['Movements'].pid_depth_get_output()
+
+    def get_depth_set_point(self):
+        return self.sensors_refs['Movements'].get_depth_set_point()
+    # end for gui
+
+    # Movements pid - yaw
+    def pid_yaw_turn_on(self):
+        self.sensors_refs['Movements'].pid_yaw_turn_on()
+
+    def pid_yaw_turn_off(self):
+        self.sensors_refs['Movements'].pid_yaw_turn_off()
+
+    def pid_hold_yaw(self):
+        self.sensors_refs['Movements'].pid_hold_yaw()
+
+    def pid_yaw_set_params(self, kp, ki, kd):
+        self.sensors_refs['Movements'].pid_yaw_set_params(kp, ki, kd)
+
+    def pid_set_yaw(self, yaw):
+        """
+        :param: yaw - float in range [-180,180] - target yaw for PID
+        """
+        self.sensors_refs['Movements'].pid_set_yaw(yaw)
+
+    # for GUI
+    def get_pid_yaw_error(self):
+        return self.sensors_refs['Movements'].pid_yaw_get_error()
+
+    def get_pid_yaw_output(self):
+        return self.sensors_refs['Movements'].pid_yaw_get_output()
+
+    def get_yaw_set_point(self):
+        return self.sensors_refs['Movements'].get_yaw_set_point()
+    # end for GUI
+
+    # Torpedoes
+    def is_torpedo_ready(self):
+        return self.sensors_refs['Torpedoes'].is_torpedo_ready()
+
+    def fire_torpedo(self):
+        self.sensors_refs['Torpedoes'].fire()
+
+    # Manipulator
+    def set_movements(self, first_param, second_param):
+        self.sensors_refs['Manipulator'].set_movements(first_param, second_param)
+
+    def close_gripper(self):
+        self.sensors_refs['Manipulator'].close_gripper()
+
+    def open_gripper(self):
+        self.sensors_refs['Manipulator'].open_gripper()
+
+    # AHRS
+    def get_rotation(self):
+        return self.sensors_refs['AHRS'].get_rotation()
+    
+    def get_yaw(self):
+        return self.sensors_refs['AHRS'].get_yaw()
+
+    def get_linear_accelerations(self):
+        return self.sensors_refs['AHRS'].get_linear_accelerations()
+
+    def get_angular_accelerations(self):
+        return self.sensors_refs['AHRS'].get_angular_accelerations()
+
+    def get_all_data(self):
+        return self.sensors_refs['AHRS'].get_all_data()
+
+    # Depth
+    def get_depth(self):
+        return self.sensors_refs['DepthSensor'].get_depth()
+
+    # Distance
+    def get_front_distance(self):
+        return self.sensors_refs['DistanceSensor'].get_front_distance()
+
+    # Hydrophones
+    def get_angle(self):
+        return self.sensors_refs['HydrophonesPair'].get_angle()
+
+    # dropper
+    def drop_marker(self):
+        self.sensors_refs['dropper'].drop()
---
+++ main_file_record.py
@@ -0,0 +1,145 @@
+'''
+Main object (thread) provides all sensors objects
+and passes them to new thread Communication.
+Communication thread is responsible of handling
+Pyro4 server and requests to it.
+
+Communication class has its own methods to all features it handles.
+It is backed by easier use of Communication class from Xavier level.
+(You don't have to know key of each sensor in sensor references dictionary)
+
+'''
+from logpy.LogPy import Logger
+import threading
+from communication.communication_record import Communication
+
+#Sensors imports
+from communication.rpi_drivers import ports
+from sensors.distance.distance import DistanceSensor
+from sensors.hydrophones.hydrophones import HydrophonesPair
+from sensors.depth.depth import DepthSensor
+from sensors.ahrs.ahrs import AHRS
+
+#Control imports
+from control.movements.movements import Movements
+from control.lights.lights import Lights
+from control.manipulator.manipulator import Manipulator
+from control.torpedoes.torpedoes import Torpedoes
+from control.dropper.dropper import Dropper
+
+from definitions import MODE, DEFLOG, SENSORS, CONTROL, RPI_ADDRESS
+
+
+class Main():
+    '''
+    Creates object of all sensor types, packs their references into
+    a list. Creates Communication thread.
+    '''
+    def __init__(self):
+        '''
+        Creates and stores references of all slave objects.
+        '''
+        self.logger = Logger(filename='main', title="Main", directory=DEFLOG.LOG_DIRECTORY)
+
+        #Sensors initialization
+        self.ahrs = None
+        self.depth = None
+        self.hydrophones = None
+        self.distance = None
+        if SENSORS.AHRS:
+            self.ahrs = AHRS(port=ports.AHRS_CLIENT_PORT,
+                             main_logger=self.logger,
+                             local_log=DEFLOG.AHRS_LOCAL_LOG,
+                             log_timing=DEFLOG.AHRS_LOG_TIMING,
+                             log_directory=DEFLOG.LOG_DIRECTORY,
+                             mode=MODE)
+        if SENSORS.DEPTH:
+            self.depth = DepthSensor(port=ports.DEPTH_CLIENT_PORT,
+                                     main_logger=self.logger,
+                                     local_log=DEFLOG.DEPTH_LOCAL_LOG,
+                                     log_timing=DEFLOG.DEPTH_LOG_TIMING,
+                                     log_directory=DEFLOG.LOG_DIRECTORY)
+        if SENSORS.HYDROPHONES:
+            self.hydrophones = HydrophonesPair(port=ports.HYDRO_CLIENT_PORT,
+                                               main_logger=self.logger,
+                                               local_log=DEFLOG.HYDROPHONES_LOCAL_LOG,
+                                               log_timing=DEFLOG.HYDROPHONES_LOG_TIMING,
+                                               log_directory=DEFLOG.LOG_DIRECTORY)
+        if SENSORS.DISTANCE:
+            self.distance = DistanceSensor(port=ports.DISTANCE_CLIENT_PORT,
+                                           main_logger=self.logger,
+                                           local_log=DEFLOG.DISTANCE_LOCAL_LOG,
+                                           log_timing=DEFLOG.HYDROPHONES_LOG_TIMING,
+                                           log_directory=DEFLOG.LOG_DIRECTORY)
+
+        #Controls initialization
+        self.lights = None
+        self.manipulator = None
+        self.torpedoes = None
+        self.dropper = None
+        self.movements = Movements(port=ports.ENGINE_SLAVE_PORT,
+                                   depth_sensor_ref=self.depth,
+                                   ahrs_ref=self.ahrs,
+                                   main_logger=self.logger,
+                                   local_log=DEFLOG.MOVEMENTS_LOCAL_LOG,
+                                   log_directory=DEFLOG.LOG_DIRECTORY)
+        if CONTROL.LIGHTS:
+            self.lights = Lights(port=ports.LIGHTS_CLIENT_PORT, main_logger=self.logger)
+        if CONTROL.MANIPULATOR:
+            self.manipulator = Manipulator(port=ports.MANIP_CLIENT_PORT, main_logger=self.logger)
+        if CONTROL.TORPEDOES:
+            self.torpedoes = Torpedoes(fire_client_port=ports.TORPEDO_FIRE_CLIENT_PORT,
+                                       ready_driver_port=ports.TORPEDO_READY_DRIVER_PORT,
+                                       main_logger=self.logger,
+                                       local_log=DEFLOG.TORPEDOES_LOCAL_LOG,
+                                       log_directory=DEFLOG.LOG_DIRECTORY)
+        if CONTROL.DROPPER:
+            self.dropper = Dropper(self.logger)
+
+        #Run threads, in control for local logers
+
+        self.logger.start()
+        if SENSORS.DEPTH:
+            self.depth.run()
+        if SENSORS.AHRS:
+            self.ahrs.run()
+        if SENSORS.HYDROPHONES:
+            self.hydrophones.run()
+        if SENSORS.DISTANCE:
+            self.distance.run()
+
+        self.movements.run()
+        if CONTROL.MANIPULATOR:
+            self.manipulator.run()
+        if CONTROL.LIGHTS:
+            self.manipulator.run()
+        if CONTROL.TORPEDOES:
+            self.torpedoes.run()
+        
+        self.sensors_refs = {
+            'AHRS':self.ahrs,
+            'DepthSensor':self.depth,
+            'HydrophonesPair':self.hydrophones,
+            'DistanceSensor':self.distance,
+            'Movements':self.movements,
+            'Lights':self.lights,
+            'Manipulator':self.manipulator,
+            'Torpedoes':self.torpedoes,
+            'dropper':self.dropper
+        }
+        #Here you can add more feature classes
+        #Remeber then to provide proper Communication class methods
+
+        self.comm = Communication(self.sensors_refs, RPI_ADDRESS, main_logger=self.logger)
+        '''
+        Communication class parameters are: sensors_refs, rpi_address,
+        main_logger, local_logger, log_directory (last three are optional)
+        '''
+
+
+
+if __name__== "__main__":
+    main = Main()
+    main.comm.start()
+    main.comm.join()
+    #St
