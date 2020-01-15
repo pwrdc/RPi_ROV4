01405b605518ba3c03da8dff7c0e5bb052dfaa63
Kacperk6 <k.krej@wp.pl>
2020-01-15 19:56:45 +0100

    fixed timestamp read from ahrs

--- sensors/ahrs/ahrs.py
+++ sensors/ahrs/ahrs.py
@@ -322,10 +322,8 @@ class AHRS_Separate():
             print("Unexpected timestamp data length")
         sampleTimeFine_bytes = data.pop(4)
 
-        print(type(sampleTimeFine_bytes))
-
         with self.lock_timestamp:
-            self.timestamp = struct.unpack(">I", sampleTimeFine_bytes)[0]
+            self.timestamp = struct.unpack("<I", sampleTimeFine_bytes)[0]
 
     def _interpret_message(self, mid: int, data: bytes):
         data_queue = BytesQueue(data)
